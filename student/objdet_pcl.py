a68011ae97ae03db550c41db55f1622ea8270b91
Prithvi <bhatprithvi@rediffmail.com>
2022-09-03 12:14:43 +0200

    uncommented statements

--- student/objdet_pcl.py
+++ student/objdet_pcl.py
@@ -148,7 +148,7 @@ def bev_from_pcl(lidar_pcl, configs):
     lidar_pcl_cpy[:, 1] = np.int_(np.floor(lidar_pcl_cpy[:, 1]/bev_map_discret)) + ((configs.bev_width + 1) / 2)
 
     # step 4 : visualize point-cloud using the function show_pcl from a previous task
-    #show_pcl(lidar_pcl_cpy)
+    show_pcl(lidar_pcl_cpy)
     
     #######
     ####### ID_S2_EX1 END #######     
@@ -182,11 +182,11 @@ def bev_from_pcl(lidar_pcl, configs):
     ## step 5 : temporarily visualize the intensity map using OpenCV to make sure that vehicles separate well from the background
     img_intensity = intensity_map * 256
     img_intensity = img_intensity.astype(np.uint8)
-    #while (1):
-    #    cv2.imshow('img_intensity', img_intensity)
-    #    if cv2.waitKey(10) & 0xFF == 27:
-    #        break
-    #cv2.destroyAllWindows()
+    while (1):
+        cv2.imshow('img_intensity', img_intensity)
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+    cv2.destroyAllWindows()
     #####
     ####### ID_S2_EX2 END ####### 
 
@@ -215,11 +215,11 @@ def bev_from_pcl(lidar_pcl, configs):
     ## step 3 : temporarily visualize the height map using OpenCV to make sure that vehicles separate well from the background
     img_height = height_map * 256
     img_height = img_height.astype(np.uint8)
-    #while (1):
-    #    cv2.imshow('img_height', img_height)
-    #    if cv2.waitKey(10) & 0xFF == 27:
-    #        break
-    #cv2.destroyAllWindows()
+    while (1):
+        cv2.imshow('img_height', img_height)
+        if cv2.waitKey(10) & 0xFF == 27:
+            break
+    cv2.destroyAllWindows()
     #######
     ####### ID_S2_EX3 END #######       
 
