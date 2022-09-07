83f06771bc975bfa0631166a6da6f595181f81cf
Prithvi <bhatprithvi@rediffmail.com>
2022-09-08 00:14:59 +0200

    step2

--- student/trackmanagement.py
+++ student/trackmanagement.py
@@ -34,22 +34,36 @@ class Track:
         # unassigned measurement transformed from sensor to vehicle coordinates
         # - initialize track state and track score with appropriate values
         ############
+        # transform measurement to vehicle coordinates
+        pos_sens = np.ones((4, 1)) # homogeneous coordinates
+        pos_sens[0:3] = meas.z[0:3] 
+        pos_veh = meas.sensor.sens_to_veh*pos_sens
 
-        self.x = np.matrix([[49.53980697],
-                        [ 3.41006279],
-                        [ 0.91790581],
-                        [ 0.        ],
-                        [ 0.        ],
-                        [ 0.        ]])
-        self.P = np.matrix([[9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 9.0e-02, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 6.4e-03, 0.0e+00, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+03, 0.0e+00],
-                        [0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 0.0e+00, 2.5e+01]])
-        self.state = 'confirmed'
-        self.score = 0
+        # save initial state from measurement
+        self.x = np.zeros((6,1))
+        self.x[0:3] = pos_veh[0:3]
+
+        # set up position estimation error covariance
+        P_pos = M_rot * meas.R * np.transpose(M_rot)
         
+        #set the covariance P velocity
+        sigma_p44 = params.sigma_p44
+        sigma_p55 = params.sigma_p55
+        sigma_p66 = params.sigma_p66
+        
+        P_vel = np.matrix([[sigma_p44**2,0,0],
+                           [0,sigma_p55**2,0],
+                           [0,0,sigma_p66**2]])
+        
+        # overall covariance initialization
+        self.P = np.zeros((6, 6))
+        self.P[0:3, 0:3] = P_pos
+        self.P[3:6, 3:6] = P_vel      
+        
+        
+        self.state = 'initialized'
+        self.score = 1.0/params.window
+                
         ############
         # END student code
         ############ 
@@ -99,17 +113,20 @@ class Trackmanagement:
         # - delete tracks if the score is too low or P is too big (check params.py for parameters that might be helpful, but
         # feel free to define your own parameters)
         ############
-        
         # decrease score for unassigned tracks
         for i in unassigned_tracks:
             track = self.track_list[i]
-            # check visibility    
+            # check visibility                
             if meas_list: # if not empty
                 if meas_list[0].sensor.in_fov(track.x):
-                    # your code goes here
-                    pass 
-
-        # delete old tracks   
+                    track.score = track.score - (1./ params.window)
+                    #pass 
+            # delete old tracks
+        for track in self.track_list:           
+            if(track.score < params.delete_threshold) and (track.state == 'confirmed'):
+                self.delete_track(track)
+            elif (track.state != 'confirmed') and (track.P[0, 0] > params.max_P or track.P[1, 1] > params.max_P):
+                self.delete_track(track)  
 
         ############
         # END student code
@@ -139,9 +156,11 @@ class Trackmanagement:
         # - increase track score
         # - set track state to 'tentative' or 'confirmed'
         ############
-
-        pass
-        
+        track.score += 1. / params.window
+        if track.score < params.confirmed_threshold:
+            track.state = 'tentative'
+        else :
+            track.state = 'confirmed'        
         ############
         # END student code
         ############ 
